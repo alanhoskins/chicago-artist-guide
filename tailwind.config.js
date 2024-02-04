fb25430c9d291c26891d5bdb8ccca2397d03d425
Alan Hoskins <alanhoskins@gmail.com>
2024-02-04 12:24:19 -0500

    add tailwind prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: ['class'],
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -7,6 +8,24 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        banana: '#F9E9B1',
+				blush: '#F7BEB2',
+				butter: '#EFC93D',
+				fog: '#F3F5FB',
+				light: '#D4D6DF',
+				dark: '#3B4448',
+				midnight: '#0C2028',
+				gold: '#F5AF19',
+				salmon: '#E17B60',
+				mint: '#82B29A',
+				slate: '#2F4550',
+				rust: '#995E4F',
+				evergreen: '#4A725D',
+				cornflower: '#537C8C',
+				yoda: '#B8D8C7',
+				sky: '#A3C2CC',
+				current: 'currentColor',
+				transparent: 'transparent',
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         btn: {
@@ -14,6 +33,11 @@ module.exports = {
           "background-hover": "hsl(var(--btn-background-hover))",
         },
       },
+      fontFamily: {
+				sans: ['Montserrat', 'sans-serif'],
+				serif: ['Lora', 'serif'],
+				main: ['"Open Sans"', 'sans-serif'],
+			},
     },
   },
   plugins: [],
